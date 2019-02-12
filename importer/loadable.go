537ee455870252aebceed8fe4fdc5f5ea7424026
John Starich <johnstarich@johnstarich.com>
2019-02-12 00:49:38 -0600

    Add basic namespacing

--- importer/loadable.go
+++ importer/loadable.go
@@ -3,7 +3,14 @@ package importer
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
+
+	"mvdan.cc/sh/syntax"
+)
+
+const (
+	functionPrefixSeparator = "-"
 )
 
 // Name is the string users invoke to execute this loadable
@@ -29,7 +36,10 @@ func Run(args []string) int {
 		fmt.Fprintln(os.Stderr, "Usage: "+UsageShort())
 		return 2
 	}
-	fmt.Println("echo", args[0])
+	if err := run(args); err != nil {
+		fmt.Fprintln(os.Stderr, "Error:", err)
+		return 1
+	}
 	return 0
 }
 
@@ -40,4 +50,100 @@ func Load(name string) int {
 
 // Unload runs any tear down required by this loadable
 func Unload() {
+}
+
+func run(args []string) error {
+	fileName := args[0]
+	parser := syntax.NewParser()
+	reader, err := os.Open(fileName)
+	if err != nil {
+		return err
+	}
+	defer reader.Close()
+	name := filepath.Base(fileName)
+	name = strings.TrimSuffix(name, filepath.Ext(name))
+	f, err := parser.Parse(reader, name)
+	if err != nil {
+		return err
+	}
+
+	extraScript := mutate(f, name)
+	fmt.Fprintf(os.Stdout, extraScript)
+	printer := syntax.NewPrinter()
+	printer.Print(os.Stdout, f)
+	return nil
+}
+
+func mutate(f *syntax.File, name string) string {
+	extraScript := ""
+	functionNames := make(map[string]bool)
+	syntax.Walk(f, func(node syntax.Node) bool {
+		switch x := node.(type) {
+		case *syntax.FuncDecl:
+			functionNames[x.Name.Value] = true
+			x.Name.Value = name + functionPrefixSeparator + x.Name.Value
+		}
+		return true
+	})
+	prefix := name + functionPrefixSeparator
+	allFunctionNames := ""
+	for name := range functionNames {
+		allFunctionNames += " " + singleQuote(name)
+	}
+
+	if !functionNames["usage"] {
+		functionNames["usage"] = true
+		extraScript += dedent(`
+			` + prefix + `usage() {
+				echo 'Usage: ` + name + ` COMMAND' >&2
+				echo 'Available commands: '` + allFunctionNames + ` >&2
+			}
+		`)
+	}
+	if !functionNames[name] {
+		extraScript += dedent(`
+			` + name + `() {
+				local subCommand=$1
+				shift
+				if [[ -z "$subCommand" ]]; then
+					` + prefix + `usage
+					return 2
+				fi
+				if ! command -v "` + prefix + `${subCommand}" >/dev/null; then
+					echo "Invalid subcommand: ${subCommand}" >&2
+					` + prefix + `usage
+					return 2
+				fi
+				"` + prefix + `${subCommand}" "$@"
+			}
+		`)
+	}
+	if !functionNames["complete"] {
+		extraScript += dedent(`
+			` + prefix + `complete() {
+				local options=(` + allFunctionNames + `)
+				local prev=${COMP_WORDS[COMP_CWORD - 1]}
+				if [[ "$prev" != ` + name + ` ]]; then
+					return
+				fi
+				COMPREPLY+=( $(compgen -W "${options[*]}" -- "${COMP_WORDS[COMP_CWORD]}") )
+			}
+		`)
+	}
+	extraScript += fmt.Sprintf("complete -F %s %s\n\n", prefix+"complete", name)
+	syntax.Walk(f, func(node syntax.Node) bool {
+		switch x := node.(type) {
+		case *syntax.CallExpr:
+			if len(x.Args) > 0 && len(x.Args[0].Parts) == 1 {
+				switch funcName := x.Args[0].Parts[0].(type) {
+				case *syntax.Lit:
+					if functionNames[funcName.Value] {
+						funcName.Value = name + functionPrefixSeparator + funcName.Value
+					}
+				}
+			}
+		}
+		return true
+	})
+	return extraScript
 }
---
+++ importer/stringutil.go
@@ -0,0 +1,47 @@
+package importer
+
+import (
+	"strings"
+)
+
+func dedent(str string) string {
+	smallestLeadingSpace := len(str)
+	lines := strings.Split(str, "\n")
+	for _, line := range lines {
+		i := 0
+		containedNonSpace := false
+		for _, c := range line {
+			switch c {
+			case rune('\t'), rune(' '):
+				i++
+			default:
+				containedNonSpace = true
+				break
+			}
+		}
+		if containedNonSpace && i < smallestLeadingSpace {
+			smallestLeadingSpace = i
+		}
+	}
+	if smallestLeadingSpace == 0 {
+		return str
+	}
+
+	result := ""
+	for _, line := range lines {
+		if len(line) < smallestLeadingSpace {
+			result += line
+		} else {
+			result += line[smallestLeadingSpace:]
+		}
+		result += "\n"
+	}
+	return result
+}
+
+func singleQuote(str string) string {
+	if !strings.Contains(str, `'`) {
+		return str
+	}
+	return `'` + strings.Replace(str, `'`, `'\''`, -1) + `'`
+}
